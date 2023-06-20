0e8d9e87c7c1f5683cc238897331bebaaf18f0d3
Emil <gasanovemil467@gmail.com>
2023-06-20 11:38:29 +0300

    Task58

---
+++ FreeCodeCamp/JavaScript Algorithms and Data Structures/Task58.js
@@ -0,0 +1,12 @@
+function trueOrFalse(wasThatTrue) {
+    if(wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+    // Only change code below this line
+
+
+
+    // Only change code above this line
+
+}
