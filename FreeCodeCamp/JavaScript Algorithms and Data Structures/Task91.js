fa20cc751a01d5499be8400ed9646dbfad8ddb83
Emil <gasanovemil467@gmail.com>
2023-06-22 09:59:17 +0300

    Task91

---
+++ FreeCodeCamp/JavaScript Algorithms and Data Structures/Task91.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Mikel Jackson",
+        "title": "Song Man",
+        "release_year": 1988,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": false
+    }
+];
