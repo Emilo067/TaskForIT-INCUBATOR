8fbc1ad93b09b4b0fc714463ed0e202d2987ffe0
Emilo067 <gasanovemil467@gmail.com>
2023-06-30 09:00:46 +0300

    SQL Basics: Simple JOIN

---
+++ CodeWars/SQL/simpleJOIN.sql
@@ -0,0 +1,4 @@
+-- Create your SELECT statement here
+SELECT products.id, products.name, isbn, company_id, price, companies.name AS company_name
+FROM products
+INNER JOIN companies ON products.company_id = companies.id
